cfc3a7792366499c78bebd13fa5135a2bd0aa4f7
M. Auliya Mirzaq Romdloni <mirzaqarjap@gmail.com>
2025-01-15 00:01:18 +0700

    feat: add album migration

---
+++ migrations/1736872279406_create-table-albums.cjs
@@ -0,0 +1,26 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+exports.shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.up = (pgm) => {
+  pgm.createTable('albums', {
+    id: { type: 'VARCHAR(50)', primaryKey: true },
+    name: { type: 'VARCHAR(255)', notNull: true },
+    year: { type: 'integer', notNull: true },
+  });
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.down = (pgm) => {
+  pgm.dropTable('albums');
+};
